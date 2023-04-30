d1a2b4f7266d0396b3ca354736fc697f7950ab7b
DmitriyVladarchuk <112862795+DmitriyVladarchuk@users.noreply.github.com>
2023-04-30 18:47:57 +0300

    Add files via upload

---
+++ task5/form.php
@@ -0,0 +1,126 @@
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Задание 5</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <style>
+/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+
+    </style>
+  </head>
+  <body>
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+
+<form action="" method="POST" class="content">
+        <h1 class="form_title">Форма</h1>
+
+        <div class="form_group <?php if ($errors['name']) {
+            print 'error';
+        } ?>">
+            <input name="name" class="form_input" placeholder=" " value="<?php print $values['name']; ?>">
+            <label class="form_label">Имя:</label>
+        </div>
+
+        <div class="form_group <?php if ($errors['email']) {
+            print 'error';
+        } ?>">
+            <input name="email" class="form_input" placeholder=" "  value="<?php print $values['email']; ?>" <?php if ($errors['email']) {
+                    print 'class="error"';
+                } ?>>
+            <label class="form_label">e-mail:</label>
+        </div>
+        
+        <div class="group">
+            <p class="text_label">Год рождения:</p>
+            <input name="datee" type="date" value="<?php print $values['datee']; ?>" <?php if ($errors['datee']) {
+                    print 'class="error"';
+                } ?>>
+        </div>
+
+        <div class="group <?php if ($errors['gender']) {
+            print 'error';
+        } ?>">
+            <p class="text_label">Пол:</p>
+            <input name = "gender" type = "radio" value= Female <?php if ($values['gender'] == 'Female') {
+                    print 'checked="checked"';
+                } ?>>Женский
+            <input name = "gender" type = "radio" value= Male <?php if ($values['gender'] == 'Male') {
+                    print 'checked="checked"';
+                } ?>>Мужской
+        </div>
+
+        <div class="group <?php if ($errors['number_limb']) {
+            print 'error';
+        } ?>">
+            <p class="text_label">Количество конечностей:</p>
+            <label class="text_label">4 </label>
+            <input class="radio" name="number_limb" type="radio" value=4 <?php if ($values['number_limb'] == 4) {
+                    print 'checked="checked"';
+                } ?>>
+            <label class="text_label">5 </label>
+            <input class="radio" name="number_limb" type="radio" value=5 <?php if ($values['number_limb'] == 5 or empty($values['limb'])) {
+                    print 'checked="checked"';
+                } ?>>
+            <label class="text_label">6 </label>
+            <input class="radio" name="number_limb" type="radio" value=6 <?php if ($values['number_limb'] == 6) {
+                    print 'checked="checked"';
+                } ?>>
+        </div>
+
+        <div class = "<?php if ($errors['superpower']) {
+            print 'error';
+        } ?>">
+            <label>
+                Cверхспособности: <br>
+                <select name="superpower[]" multiple="multiple">
+                    <option  value = 1 <?php if (isset($_COOKIE["1"])) if ($_COOKIE["1"]=="true") echo "selected" ?> > Бессмертие </option>
+                    <option  value = 2 <?php if (isset($_COOKIE["2"])) if ($_COOKIE["2"]=="true") echo "selected" ?> > Прохождение сквозь стены </option>
+                    <option  value = 3 <?php if (isset($_COOKIE["3"])) if ($_COOKIE["3"]=="true") echo "selected" ?> > Левитация </option>
+                </select><br>
+            </label>
+            <br>
+        </div>
+
+        <div class="group <?php if ($errors['biography']) {
+            print ' error';
+        } ?>">
+            <textarea name = "biography" placeholder = "Биография" class = "form_textarea"> <?php print $values['biography']; ?> </textarea>
+        </div>
+        
+        <div>
+            <label><input name = "Contract" type = "checkbox" value = 1 <?php if ($values['Contract']) {
+                    print 'checked="checked"';
+                } ?>>С контрактом ознакомлен(а)</label>
+            <br>
+        </div>
+        <input type = "submit" value = "Отправить данные" class = "form_btn">
+
+        <br>
+        <br>
+        <div class="group">
+            <a href = "login.php" class="labelInput"> <center>Войти</center> </a>
+            <?php
+            if (!empty($messages))
+            {
+                if (!empty($_SESSION['login']))
+                printf($_SESSION['login'], $_SESSION['uid']);
+            }
+            ?>
+        </div>
+        
+    </form>
+  </body>
+</html>
---
+++ task5/login.php
@@ -0,0 +1,69 @@
+<?php
+    header('Content-Type: text/html; charset=UTF-8');
+
+    session_start();
+
+    if (!empty($_SESSION['login']))
+    {
+        session_destroy();
+        header('Location: ./');
+    }
+    if ($_SERVER['REQUEST_METHOD'] == 'GET')
+    {
+        ?>
+
+        <head>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="styles.css">
+        </head>
+        <body>
+            <form action = "" method = "POST" class="content">
+                <h1 class="form_title">Авторизация</h1>
+                <div class="form_group">
+                    <input name="name" class="form_input" placeholder=" ">
+                    <label class="form_label">Логин:</label>
+                </div>
+                <div class="form_group">
+                    <input name="name" class="form_input" placeholder=" ">
+                    <label class="form_label">Пароль:</label>
+                </div>
+                <!-- <input name = "user_login" type = "text" placeholder = "Логин" class = "txtb sf_input">
+                <input name = "user_password" type = "text" placeholder = "Пароль" class = "txtb sf_input"> -->
+                <input type = "submit" value = "Войти" class = "form_btn">
+            </form>
+        </body>
+
+        <?php
+        if (!empty($_GET['none']))
+        {
+            $message = "Неверные данные!";
+            print($message);
+        }
+    }
+    else
+    {
+        $user = 'u52869';
+        $password = '6068422';
+        $database = new PDO('mysql:host=localhost;dbname=u52869', $user, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+        $user_login = $_POST['user_login'];
+        $user_password = md5($_POST['user_password']);
+        $statement = $database -> prepare("SELECT id_login FROM user_info WHERE user_login = ? AND user_password = ?");
+        $statement -> execute([$user_login, $user_password]);
+        $user_id = $statement -> fetch(PDO::FETCH_COLUMN);
+
+        if ($user_id)
+        {
+            $_SESSION['login'] = $_POST['user_login'];
+            $_SESSION['uid'] = $user_id;
+            $_COOKIE[session_name()] = "session_true";
+            header('Location: ./');
+        }
+
+        else
+        {
+            header('Location: ./');
+        }
+    }
+?>
